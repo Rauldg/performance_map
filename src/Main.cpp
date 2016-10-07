76cd487921a2ae766501eeff9affe9ae4aa6d2e3
Raul Dominguez <raul.dominguez@dfki.de>
2016-10-07 15:24:16 +0200

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <performance_map/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    performance_map::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
